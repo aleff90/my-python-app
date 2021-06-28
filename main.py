cc5ed80b39b15960e9e08b2786c8b0ddc1df995d
aleff90 <thommy90vercetti@gmail.com>
2021-06-27 21:08:11 -0300

    learning python

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello Amigoscode")
+
+name, age = "Jamila", 20
+pi = 3.14
+number = [1, 2, 3, 4]
+
+print(name)
+print(age)
+print(pi)
+print(number)
+
+brand = "Amigoscode"
+age = 2
+pi = 3.14
+numbers = []
+print(type(brand))
+print(type(age))
+print(type(numbers))
+print(type(pi))
